576678624102a676960002eb4a7c36fa6ba344cd
Jan <Jan@TERMINATOR>
2017-10-22 22:27:12 +0200

    Mandelbrot yay

--- CSets/src/Main.java
+++ CSets/src/Main.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelWriter;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -13,10 +14,19 @@ public class Main extends Application{
 	private GraphicsContext gc;
 	private PixelWriter pw;
 	private Color colBackground = Color.ALICEBLUE;
-	private int iterations = 0;
-	private static double limit = 2;
 	private long lastNanoTime;
 	private double timer = 0;
+	
+	private double scale = 200;
+	private int iterations = 1000;
+	private static double limit = 2;
+	Complex julia = new Complex(0, 0.4);
+		
+	private double x = 0;
+	private double y = 0;
+	
+	private int resX = 800;
+	private int resY = 800;
 	
 
 	public static void main(String[] args) {
@@ -30,10 +40,11 @@ public class Main extends Application{
 		Scene scene = new Scene(root, 900, 900);
 		primaryStage.setScene(scene);
 		
-		canvas = new Canvas(800, 800);
+		canvas = new Canvas(resX, resY);
 		gc = canvas.getGraphicsContext2D();
 		pw = canvas.getGraphicsContext2D().getPixelWriter();
 		root.setCenter(canvas);
+		canvas.setOnMouseClicked(e -> onClickScreen(e));
 
 		primaryStage.show();
 		
@@ -52,72 +63,102 @@ public class Main extends Application{
 	        	  refreshScreen(dt);
 	          }
 	      }.start();
+	      
+	      calcSet();
 	}
 	
-	public void setPixel(int x, int y, Color c) {
-		pw.setColor(x, y, c);
+	private void onClickScreen(MouseEvent e) {
+		x = x + (e.getX() - resX/2)/scale;
+		y = y - (e.getY() - resY/2)/scale;
+		scale *= 1.5;
+		System.out.println(x + " " + y);
+		
+		calcSet();
+	}
+	
+	public void calcSet() {
+		for (double i = 0; i < resY; i++) {
+	  		for (double j = 0; j < resX; j++) {
+	  			
+				double wx = x + (j - resX/2)/scale;
+				double wy = y - (i - resY/2)/scale;
+				
+				
+				int value = iterator(new Complex(wx, wy));
+				if (value == 0) setPixel((int) j, (int) i, Color.BLACK);
+				else {
+					double colSteps = 10;
+					setPixel((int) j, (int) i, Color.color(
+							1- Math.pow(Math.sin(Math.sqrt(value)/colSteps),2),
+							1- Math.pow(Math.sin(Math.sqrt(value)/colSteps + Math.PI * 0.66666667),2),
+							1- Math.pow(Math.sin(Math.sqrt(value)/colSteps + Math.PI * 1.33333333),2)
+							));
+				}
+			}
+		}
+	    
+		//CalcThread T2 = new CalcThread( "Thread-2");
+	    //T2.start(1, resX, resY, x, y, scale, pw1);
+	    
+		//CalcThread T3 = new CalcThread( "Thread-3");
+	    //T3.start(2, resX, resY, x, y, scale, pw2);
+		
 	}
 	
 	public void refreshScreen(double dt) {
 		timer += dt;
 		if (timer > 0) {
-			iterations++;
-			timer -= 0.5;
-			for (double i = 0; i < 800; i++) {
-				for (double j = 0; j < 800; j++) {
-					//int value = iterator(new Complex(j/600000 +0.14, 0.5875 -(i/600000)));
-					int value = iterator(new Complex(j/200 -2, 2 -(i/200)));
-					if (value == 0) setPixel((int) j, (int) i, Color.BLACK);
-					else {
-						setPixel((int) j, (int) i, Color.color(1, 1- (double) value/iterations, 1 - (double) value/iterations));
-					}
-				}
-			}
+			timer -= 0.2;
+			julia.y += 0.001;
+			calcSet();
 		}
 	}
+	
+	 public int iterator(Complex c) {
+			//Julia Set
+			//Complex z = c;
+			//====================
+			
+			//Madelbrot Set
+			Complex z = new Complex(0, 0);
+			//====================
+			
+			Complex buffer;
+			int i;
+			for (i = 0; i < iterations; i++) {
+				
+				//Julia Set
+				//buffer = function(z, julia);
+				//====================
+				
+				//Madelbrot Set
+				buffer = function(z, c);
+				//====================
+				
+				
+				if (buffer.add(z.neg()).abs() == 0) break;
+				z = buffer;
+				if (z.abs() > limit) break;
+				//System.out.println(z);
+			}
+			
+			if (z.abs() < limit) {
+				return 0;
+			}
+			return i;
+		}
+		
+		public Complex function(Complex z, Complex c) {
+			return z.mul(z).add(c);
+		}
+		
+		public void setPixel(int x, int y, Color c) {
+			pw.setColor(x, y, c);
+		}
 	
 	public void clearScreen() {
 		gc.setFill(colBackground);
 		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 	}
 	
-	public int iterator(Complex c) {
-		//Julia Set
-		//Complex z = c;
-		//Complex r = new Complex(0.3, -0.49);
-		//====================
-		
-		//Madelbrot Set
-		Complex z = new Complex(0, 0);
-		//====================
-		
-		Complex buffer;
-		int i;
-		for (i = 0; i < iterations; i++) {
-			
-			//Julia Set
-			//buffer = function(z, r);
-			//====================
-			
-			//Madelbrot Set
-			buffer = function(z, c);
-			//====================
-			
-			
-			if (buffer.add(z.neg()).abs() == 0) break;
-			z = buffer;
-			if (z.abs() > limit) break;
-			//System.out.println(z);
-		}
-		
-		if (z.abs() < limit) {
-			return 0;
-		}
-		return i;
-	}
-	
-	public Complex function(Complex z, Complex c) {
-		return z.mul(z).add(c);
-	}
-
 }
